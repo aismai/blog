b7241df5efe90cc504d206378eb708cf0d0471b4
aismai <abai.ismailov@gmail.com>
2016-10-14 10:17:16 +0600

    generate blog/index, blog/new routes

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('blogs');
+  this.route('blogs', function() {
+    this.route('new');
+  });
 });
 
 export default Router;
