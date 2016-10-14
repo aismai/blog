10dd6b2ef7925d1568d14e8805a12b6ed6a99e0c
aismai <abai.ismailov@gmail.com>
2016-10-14 10:51:50 +0600

    install Ember-localstorage-adapter

--- app/routes/blogs/new.js
+++ app/routes/blogs/new.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return this.store.createRecord('blog');
+  },
+  actions: {
+    save(blog) {
+      blog.save().then(() => {
+        this.transitionTo('blogs');
+      });
+    },
+
+    willTransition() {
+      this.controller.get('model').rollbackAttributes();
+    }
+  }
 });
