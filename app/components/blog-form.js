c9d85308c770d481c66bb9a912b8be9cf0ca30b6
aismai <abai.ismailov@gmail.com>
2016-11-03 14:13:03 +0600

    edit blog-form component

--- app/components/blog-form.js
+++ app/components/blog-form.js
@@ -1,12 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  blog: undefined,
+  blogTypeList: Ember.computed(function () {
+    return this.get('store').findAll('blog-type');
+  }),
+
+  // init() {}, willInsertElement() {}, action-save();
+  init() {
+    this._super(...arguments);
+    const blog = this.get('item') || this.get('store').createRecord('blog', {
+        user: this.get('authManager.currentUser'),
+      });
+    this.set('blog', blog);
+  },
 
   actions: {
-
-    //TODO: rename 'param' in all components
-    buttonClicked(param) {
-      this.sendAction('action', param);
+    buttonClicked(blog) {
+      this.sendAction('action', blog);
     }
   }
 });
