f12e62eac1756fa8b8449b9aba29f5127d8de699
aismai <abai.ismailov@gmail.com>
2016-11-04 14:49:46 +0600

    add last login computed property

--- app/models/user.js
+++ app/models/user.js
@@ -1,14 +1,29 @@
 import DS from 'ember-data';
 import Ember from 'ember';
+import moment from 'moment';
 
 export default DS.Model.extend({
   email: DS.attr('string'),
   password: DS.attr('string'),
-  registrationDate: DS.attr('date'),
-  lastLogin: DS.attr('date'),
+  registrationDate: DS.attr('date', {
+    defaultValue() {
+      return new Date();
+    }
+  }),
+
+  login: DS.attr('date'),
 
   blogs: DS.hasMany('blog'),
   posts: DS.hasMany('post'),
   comments: DS.hasMany('comment'),
   isValid: Ember.computed.notEmpty('body'),
+
+
+  registration: Ember.computed(function () {
+    return moment(this.get('registrationDate')).format("DD-MM-YYYY")
+  }),
+
+  lastLogin: Ember.computed('login', function () {
+    return moment(this.get('login')).format("DD-MM-YYYY, HH:mm:ss")
+  })
 });
--- app/routes/users/new.js
+++ app/routes/users/new.js
@@ -2,9 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.createRecord('user', {
-      registration: new Date()
-    });
+    return this.store.createRecord('user');
   },
 
   actions: {
