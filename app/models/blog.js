b234b5991812911087b80f455b558d6983aa704e
aismai <abai.ismailov@gmail.com>
2016-11-28 10:25:32 +0600

    add ember-cli-cropper
    generate image-cropper component
    add cover field for blog

--- app/models/blog.js
+++ app/models/blog.js
@@ -5,13 +5,14 @@ import Ember from 'ember';
 export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
+  cover: DS.attr('string'),
 
   posts: DS.hasMany('post'),
   user: DS.belongsTo('user'),
   blogType: DS.belongsTo('blog-type'),
 
   isValid: Ember.computed.notEmpty('name'),
-  // isAuthor: Ember.computed('user.id', function () {
-  //   return (this.get('user.id') === this.get('authManager.currentUser.id'));
-  // })
+  isAuthor: Ember.computed('user.id', function () {
+    return (this.get('user.id') === this.get('authManager.currentUser.id'));
+  })
 });
