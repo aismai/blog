ee709b2a4bc2e9a9b1144456250048db215ec0af
aismai <abai.ismailov@gmail.com>
2016-12-01 13:25:25 +0600

    reformat roles to pods structure

--- app/pods/roles/index/route.js
+++ app/pods/roles/index/route.js
@@ -7,9 +7,12 @@ export default Ember.Route.extend({
 
   actions: {
     deleteRole(role) {
+      // TODO: if user has no roles? .includes throws error
       const usersPromise = this.store.findAll('user');
+      console.log(role.get('id'));
       usersPromise.then((users) => {
         users.forEach((user) => {
+          console.log(user.get('email'));
           if (user.get('role.id')
                   .includes(role.get('id'))) {
             user.set('role', null);
