b5c2ea874815b5c486e2cc9b20b460895af4cd52
aismai <abai.ismailov@gmail.com>
2016-12-17 10:44:36 +0600

    add main-filter component

---
+++ app/pods/components/main-filter/component.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  filterService: Ember.inject.service('filter-service'),
+  registeredFilters: [],
+
+  actions: {
+    registerFilter(filter) {
+      this.get('registeredFilters')
+          .pushObject(filter);
+    },
+
+    runFilters() {
+      this.get('filterService').setBlogs(this.get('filterService.blogs'));
+      this.get('registeredFilters')
+          .forEach((filter) => {
+            filter.run();
+            }
+          );
+    },
+  }
+});
