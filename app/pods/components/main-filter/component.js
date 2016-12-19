c3e4600184ad060acc24d49284e9efe87e558659
aismai <abai.ismailov@gmail.com>
2016-12-19 09:04:26 +0600

    add styles for filter components
    move filters to main-filter component

--- app/pods/components/main-filter/component.js
+++ app/pods/components/main-filter/component.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: '',
   filterService: Ember.inject.service('filter-service'),
   registeredFilters: [],
 
--- app/pods/components/search-filter/component.js
+++ app/pods/components/search-filter/component.js
@@ -1,36 +1,33 @@
-import Ember from 'ember';
 import BaseFilter from '../base-filter/component';
 
 export default BaseFilter.extend({
-    tagName:       '',
+    tagName: '',
 
     run () {
       const searchParam = this.get('filterValue');
       if (searchParam) {
-          const filteredBlogs = this.get('filterService.filteredBlogs')
-                                    .filter((blog) => {
-                                      const blogName        = blog.get('name');
-                                      const blogDescription = blog.get('description');
+        const filteredBlogs = this.get('filterService.filteredBlogs')
+                                  .filter((blog) => {
+                                    const blogName        = blog.get('name');
+                                    const blogDescription = blog.get('description');
 
-                                      const findByName = blogName && blogName
+                                    const findByName = blogName && blogName
+                                        .toUpperCase()
+                                        .includes(searchParam
                                           .toUpperCase()
-                                          .includes(searchParam
-                                            .toUpperCase()
-                                          );
+                                        );
 
-                                      const findByDescription = blogDescription && blogDescription
+                                    const findByDescription = blogDescription && blogDescription
+                                        .toUpperCase()
+                                        .includes(searchParam
                                           .toUpperCase()
-                                          .includes(searchParam);
+                                        );
 
-                                      return findByName || findByDescription;
-                                    });
-          this.get('filterService')
-              .setBlogs(filteredBlogs);
-        }
-        // else {
-        //   // this.get('filterService')
-        //   //     .setBlogs(this.get('filterService.blogs'));
-        // }
+                                    return findByName || findByDescription;
+                                  });
+        this.get('filterService')
+            .setBlogs(filteredBlogs);
       }
+    }
   }
 );
