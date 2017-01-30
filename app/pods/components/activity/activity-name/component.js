48b850196137dfa39188e32a3016233db8294b99
aismai <abai.ismailov@gmail.com>
2017-01-30 13:46:56 +0600

    refactor activity-types

--- app/pods/components/activity/activity-name/component.js
+++ app/pods/components/activity/activity-name/component.js
@@ -3,7 +3,25 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   activityType: Ember.computed('item',
     function () {
-      return `activity/${this.get('item.type')}`;
+      // console.log(this.get('item.type').includes('type-of-create'));
+      return this.returnActivityType(this.get('item'));
+      // return `activity/${this.get('item.type')}`;
     }
-  )
+  ),
+
+  returnActivityType(item) {
+    const activityType = {
+      'blog-create':    'type-of-create',
+      'blog-edit':      'type-of-edit',
+      'blog-delete':    'type-of-delete',
+      'post-create':    'type-of-create',
+      'post-edit':      'type-of-edit',
+      'post-delete':    'type-of-create',
+      'comment-create': 'type-of-create',
+      'comment-edit':   'type-of-edit',
+      'comment-delete': 'type-of-create',
+    };
+    return `activity/${activityType[item.get('type')]}`;
+  }
+
 });
